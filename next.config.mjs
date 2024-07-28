c0a16c99df4561c22390307af0537953da6d62c1
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-07-28 14:55:48 +0530

    add image host

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "notion.so",
+      },
     ],
   },
   redirects: async () => {

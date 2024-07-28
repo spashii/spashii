33f17873083247ff048031c99b3b61a25f01f138
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-07-28 14:24:29 +0530

    finish the first fully working model!

--- src/app/tag/[slug]/page.tsx
+++ src/app/tag/[slug]/page.tsx
@@ -1,9 +1,13 @@
 import { BlogPostPreviewGrid } from "@/components/BlogPostPreview";
 
 import { Badge } from "@/components/ui/badge";
+import { getAllBlogPosts } from "@/lib/notion";
 import { wisp } from "@/lib/wisp";
 import { CircleX } from "lucide-react";
 import Link from "next/link";
+
+export const dynamic = "force-static";
+export const revalidate = 1800;
 
 interface Params {
   slug: string;
@@ -27,17 +31,21 @@ const Page = async ({
   params: Params;
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const page = searchParams.page ? parseInt(searchParams.page as string) : 1;
-  const result = await wisp.getPosts({ limit: 6, tags: [slug], page });
+  const allPosts = await getAllBlogPosts();
+
+  const results = allPosts.filter((post) =>
+    post.tags.some((tag) => tag.name === slug)
+  );
+
   return (
     <div>
       <Link href="/">
-        <Badge className="px-2 py-1">
+        <Badge className="px-2 py-1 my-6">
           <CircleX className="inline-block w-4 h-4 mr-2" />
           Posts tagged with <strong className="mx-2">#{slug}</strong>{" "}
         </Badge>
       </Link>
-      <BlogPostPreviewGrid posts={result.posts} />
+      <BlogPostPreviewGrid posts={results} />
     </div>
   );
 };
--- src/app/tag/page.tsx
+++ src/app/tag/page.tsx
@@ -1,10 +1,10 @@
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import Layout from "@/components/Layout";
 import { config } from "@/config";
+import { getAllBlogPosts } from "@/lib/notion";
 import { signOgImageUrl } from "@/lib/og-image";
-import { wisp } from "@/lib/wisp";
 import Link from "next/link";
+
+export const dynamic = "force-static";
+export const revalidate = 1800;
 
 export async function generateMetadata() {
   return {
@@ -24,24 +24,31 @@ export async function generateMetadata() {
 }
 
 export default async function Page() {
-  const result = await wisp.getTags();
+  const allPosts = await getAllBlogPosts();
+
+  const tags = new Set<string>();
+  allPosts.forEach((post) => {
+    post.tags.forEach((tag) => tags.add(tag.name));
+  });
 
   return (
     <div>
-      <div className="text-center">
+      <div className="">
         <h1 className="mb-2 text-5xl font-bold">Tags</h1>
         <p className="text-lg opacity-50">List of all tags</p>
       </div>
-      <div className="my-10 max-w-6xl text-balance text-center text-xl mb-48">
-        {result.tags.map((tag) => (
-          <Link
-            key={tag.id}
-            href={`/tag/${tag.name}`}
-            className="text-primary mr-2 inline-block"
-          >
-            #{tag.name}
-          </Link>
-        ))}
+      <div className="my-10 max-w-6xl text-balance text-center text-xl mb-48 flex gap-8 wrap">
+        {Array.from(tags)
+          .sort()
+          .map((tag) => (
+            <Link
+              key={tag}
+              href={`/tag/${tag}`}
+              className="text-primary inline-block"
+            >
+              #{tag}
+            </Link>
+          ))}
       </div>
     </div>
   );

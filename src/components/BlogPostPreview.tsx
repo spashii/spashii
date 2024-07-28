66ed60ec1f625951923c4e4bbc91cca02f1b0e33
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2024-07-29 03:32:18 +0530

    link the card

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -21,33 +21,35 @@ export const BlogPostPreview: FunctionComponent<{
           />
         </div>
       </Link>
-      <div className="grid grid-cols-1 gap-2 md:col-span-2 mt-4 p-4">
-        <h2
-          className={cn(
-            "font-sans font-semibold tracking-tighter text-primary",
-            !small ? "text-2xl md:text-3xl" : "text-xl"
-          )}
-        >
-          <Link href={`/journal/${post.slug}`}>{post.title}</Link>
-        </h2>
-        <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
-          {formatDate(post.publishedAt || post.updatedAt, "dd MMMM yyyy")}
+      <Link href={`/journal/${post.slug}`}>
+        <div className="grid grid-cols-1 gap-2 md:col-span-2 mt-4 p-4">
+          <h2
+            className={cn(
+              "font-sans font-semibold tracking-tighter text-primary",
+              !small ? "text-2xl md:text-3xl" : "text-xl"
+            )}
+          >
+            {post.title}
+          </h2>
+          <div className="prose lg:prose-lg italic tracking-tighter text-muted-foreground">
+            {formatDate(post.publishedAt || post.updatedAt, "dd MMMM yyyy")}
+          </div>
+          <div
+            className={cn(
+              "prose lg:prose-lg leading-relaxed text-sm md:text-lg line-clamp-4 text-muted-foreground",
+              small ?? "!text-sm"
+            )}
+          >
+            {post.description}
+          </div>
         </div>
-        <div
-          className={cn(
-            "prose lg:prose-lg leading-relaxed text-sm md:text-lg line-clamp-4 text-muted-foreground",
-            small ?? "!text-sm"
-          )}
-        >
-          {post.description}
-        </div>
-        <div className="text-sm text-muted-foreground flex flex-wrap gap-2">
-          {post.tags.map((tag) => (
-            <div key={tag.id} className="inline-block">
-              <Link href={`/tag/${tag.name}`}>#{tag.name}</Link>
-            </div>
-          ))}
-        </div>
+      </Link>
+      <div className="text-sm text-muted-foreground flex flex-wrap gap-2 p-4">
+        {post.tags.map((tag) => (
+          <div key={tag.id} className="inline-block">
+            <Link href={`/tag/${tag.name}`}>#{tag.name}</Link>
+          </div>
+        ))}
       </div>
     </div>
   );
